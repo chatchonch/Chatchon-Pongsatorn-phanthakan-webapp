e312db0c2cf951bb273b85a219928efb79b2240c
Pongsatorn-Sudputong <68942316+Pongsatorn-Sudputong@users.noreply.github.com>
2020-12-07 04:24:45 +0700

    Add files via upload

--- scripts/list_name.js
+++ scripts/list_name.js
@@ -1,8 +1,5 @@
 window.onload = function() {
-    let SSN = document.getElementById("ssn_to_find")
-    let searchBtn = document.getElementById("searchBtn");
-
-    var firebaseConfig = {
+  var firebaseConfig = {
       apiKey: "AIzaSyBsm2OONg2-y0A53i0mhDrGaBv_EzNksRE",
       authDomain: "myprojectpcu.firebaseapp.com",
       projectId: "myprojectpcu",
@@ -10,34 +7,31 @@ window.onload = function() {
       messagingSenderId: "653860991694",
       appId: "1:653860991694:web:09b7de81d7b5f15d2b569f",
       measurementId: "G-CB5P88YJ8G"
-    };
-
-    firebase.initializeApp(firebaseConfig);
-    
-    var db = firebase.firestore();
-    db.collection("สถานบริการที่ 1")
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
-    searchBtn.addEventListener('click', function() {
-      SSN = SSN.value;
-      db.collection("สถานบริการที่ 1").doc(SSN).get().then(function(doc) {
-        if (doc.exists) {
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+  db.collection("สถานบริการที่ 1").doc("1409901234567").get().then(function(doc) {
+      if (doc.exists) {
           console.log("Document data:", doc.data());
-        }
-        else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      }).catch(function(error) {
-        console.log("Error getting document:", error);
-});
-    })
-}
+          console.log(doc.data().firstname + " "+ doc.data().lastname); 
+          console.log(doc.data().dateAppointment)
+          console.log(doc.data().timeAppointment);
+          if (doc.data().timeAppointment[3] = 3){
+              var end_time = doc.data().timeAppointment[0] + doc.data().timeAppointment[1] + ':' + '45';
+              console.log(end_time)
+              result = {
+                  title: `${doc.data().firstname} ${doc.data().lastname}`,
+                  start: `${doc.data().dateAppointment}T${doc.data().timeAppointment}`,
+                  end: `${doc.data().dateAppointment}T${end_time}`
+              }
+              calendar(result);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+  }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+  }
--- scripts/selectPlace.js
+++ scripts/selectPlace.js
@@ -8,10 +8,6 @@ window.onload = function() {
     document.querySelector(".bg-timeModal").style.display = "none";
     });
 
-    var client = document.getElementById("clientButton");
-    client.addEventListener("click", function() {
-    document.querySelector(".bg-clientModal").style.display = "flex";
-    });
     var close_timerChange = document.querySelector(".close-client")
     close_timerChange.addEventListener("click", function() {
     document.querySelector(".bg-clientModal").style.display = "none";
